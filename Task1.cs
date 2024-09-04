ddcac160abe12c466f3949ed525d60c53abf5d46
Volodymyr Semkovych <37297861+vovasemkovych@users.noreply.github.com>
2024-09-03 20:49:13 -0400

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Task1Event
+    {
+        public delegate void ButtonClickHandler(object sender, EventArgs e);
+        public event ButtonClickHandler ButtonClick;
+        public void OnClick() {
+            ButtonClick(this, EventArgs.Empty);
+        }
+    }
+    public class Taks1Handle {
+        private static void Button_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button was clicked!");
+        }
+
+        public static void Main()
+        {
+            Task1Event button = new Task1Event();
+            button.ButtonClick += Button_Click;
+            button.OnClick();
+        }
+    }
+}
